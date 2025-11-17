a009d5fa84f478e3b702aa95f6f19075dbf8cfd5
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-17 16:01:56 +0700

    Update train_synth.py

--- gram/scripts/train_synth.py
+++ gram/scripts/train_synth.py
@@ -12,7 +12,7 @@ import torch.optim as optim
 from gram.model.gram import GRAM, load_tree, pad_batch
 
 
-SEQ_FILE = "gram/data/synthetic_converted/synthetic.seqs"
+SEQ_FILE = "gram/data/synthetic_converted/synthetic_mapped.seqs"
 TREE_PREFIX = "gram/data/mimic3_tree"
 
 MODEL_OUT = "gram/data/synth_train.pt"
