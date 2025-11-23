a2722cb1a395c6430e95ddfc3082040f03721bfa
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-23 21:44:05 +0700

    Update mamba_synthetic.py

--- mamba/mamba_synthetic.py
+++ mamba/mamba_synthetic.py
@@ -26,8 +26,8 @@ class SyntheticEHRMamba(nn.Module):
                 d_conv=d_conv,
                 expand=2,
                 headdim=64,  # Giảm để nhẹ hơn
-                chunk_size=64,              # Thêm dòng này (giảm từ 256 → 64)
-                use_mem_eff_path=False,
+                chunk_size=256,             
+                use_mem_eff_path=True,
                 layer_idx=i,
             ) for i in range(n_layer)
         ])
