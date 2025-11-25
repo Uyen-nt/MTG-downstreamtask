76be0e0310a11fa8cb705595014fbc73d14eb593
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-25 11:42:11 +0700

    Create dataset.py

---
+++ retain_micron/dataset.py
@@ -0,0 +1,25 @@
+# retain_micron/dataset.py
+from torch.utils.data import Dataset
+import torch
+
+class EHRDataset(Dataset):
+    def __init__(self, sequences, labels, n_codes):
+        self.sequences = sequences
+        self.labels = labels
+        self.n_codes = n_codes
+
+    def __len__(self):
+        return len(self.sequences)
+
+    def __getitem__(self, idx):
+        return self.sequences[idx], self.labels[idx]
+
+def collate_fn(batch, n_codes):
+    sequences, labels = zip(*batch)
+    labels_onehot = []
+    for lbl in labels:
+        onehot = torch.zeros(n_codes)
+        if len(lbl) > 0:
+            onehot[list(lbl)] = 1.0
+        labels_onehot.append(onehot)
+    return list(sequences), torch.stack(labels_onehot)
