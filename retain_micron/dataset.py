36cf5c2f4a3d9b6569348bfb3ee04efea1a918f7
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-26 20:42:12 +0700

    Update dataset.py

--- retain_micron/dataset.py
+++ retain_micron/dataset.py
@@ -18,24 +18,28 @@ class EHRDataset(Dataset):
 def collate_fn(batch, n_codes=2869):
     sequences, labels = zip(*batch)
     batch_size = len(labels)
+    
+    print(f"DEBUG - Real batch size: {batch_size}")
+    print(f"DEBUG - First sequence: {len(sequences[0])} visits")
+    print(f"DEBUG - First label: {labels[0][:5]}...")
+    
     labels_onehot = torch.zeros(batch_size, n_codes)
 
     for i, lbl in enumerate(labels):
         for code in lbl:
             if isinstance(code, (int, np.integer)) and 0 <= code < n_codes:
                 labels_onehot[i, code] = 1.0
+    
     return list(sequences), labels_onehot
 
-# def collate_fn(batch):
+# def collate_fn(batch, n_codes=2869):
 #     sequences, labels = zip(*batch)
-#     labels_onehot = []
-#     for lbl in labels:
-#         onehot = torch.zeros(2869)  # n_codes = 2869, indices từ 0-2868
-        
-#         # Lọc các index hợp lệ (0-2868)
-#         valid_indices = [idx for idx in lbl if idx < 2869]
-#         if valid_indices:
-#             onehot[valid_indices] = 1.0
-#         labels_onehot.append(onehot)
-    
-#     return list(sequences), torch.stack(labels_onehot)
+#     batch_size = len(labels)
+#     labels_onehot = torch.zeros(batch_size, n_codes)
+
+#     for i, lbl in enumerate(labels):
+#         for code in lbl:
+#             if isinstance(code, (int, np.integer)) and 0 <= code < n_codes:
+#                 labels_onehot[i, code] = 1.0
+#     return list(sequences), labels_onehot
+
