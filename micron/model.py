3636b3a4f2ca1fb4db5e6d93f196c2751a8772b7
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-27 18:08:30 +0700

    Update model.py

--- micron/model.py
+++ micron/model.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 class MICRON_DX(nn.Module):
     def __init__(self, vocab_size, emb_dim=256, device=torch.device('cpu')):
         super(MICRON_DX, self).__init__()
@@ -6,15 +9,13 @@ class MICRON_DX(nn.Module):
 
         # Embedding cho diagnosis codes
         self.embedding = nn.Embedding(vocab_size, emb_dim)
-        self.dropout = nn.Dropout(p=0.5)
+        self.dropout = nn.Dropout(p=0.3)
 
-        # health representation
         self.health_net = nn.Sequential(
             nn.Linear(emb_dim, emb_dim),
             nn.ReLU(),
         )
 
-        # dự đoán diagnoses cho visit tiếp theo
         self.predictor = nn.Sequential(
             nn.Linear(emb_dim, emb_dim * 2),
             nn.ReLU(),
@@ -23,39 +24,31 @@ class MICRON_DX(nn.Module):
 
         self.init_weights()
 
-    def forward(self, input):
+    def forward(self, visits):
         """
-        input dạng:
-        [
-            [codes of visit 1],
-            [codes of visit 2],
-            ...
-        ]
-        L-1 visits 
-        predict visit L
+        visits: list[ list[int] ]
+        history: visits[:-1]
+        label: visits[-1]
         """
 
         # visit hiện tại (t)
-        diag_emb_t = self.embedding(torch.LongTensor(input[-1]).to(self.device))  
-        diag_emb_t = diag_emb_t.mean(dim=0, keepdim=True)   # (1, dim)
+        diag_emb_t = self.embedding(torch.LongTensor(visits[-1]).to(self.device))  
+        diag_emb_t = diag_emb_t.mean(dim=0, keepdim=True)   
 
-        # visit trước đó (t-1), nếu không có → zeros
-        if len(input) < 2:
+        # visit trước đó (t-1)
+        if len(visits) < 2:
             diag_emb_prev = torch.zeros_like(diag_emb_t)
         else:
-            diag_emb_prev = self.embedding(torch.LongTensor(input[-2]).to(self.device))
+            diag_emb_prev = self.embedding(torch.LongTensor(visits[-2]).to(self.device))
             diag_emb_prev = diag_emb_prev.mean(dim=0, keepdim=True)
 
-        # transform
-        h_t = self.health_net(diag_emb_t)       # (1,dim)
-        h_prev = self.health_net(diag_emb_prev) # (1,dim)
+        h_t = self.health_net(diag_emb_t)       
+        h_prev = self.health_net(diag_emb_prev) 
 
         # residual learning
         h_res = h_t - h_prev
 
-        # predict diagnoses
-        logits = self.predictor(h_res)  # (1, vocab)
-
+        logits = self.predictor(h_res)  
         return logits
 
     def init_weights(self):
