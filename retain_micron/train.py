fe989572e55639772f00af3e14a3b7c96238c616
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-11-27 10:31:17 +0700

    Update train.py

--- retain_micron/train.py
+++ retain_micron/train.py
@@ -38,8 +38,9 @@ def train_model(model, train_loader, val_loader, epochs, save_path):
     class_weights = class_weights.to(device)
     
     # Sử dụng Focal Loss hoặc Weighted BCE Loss
-    criterion = FocalLoss(alpha=0.75, gamma=0.5)
-    # Hoặc: criterion = nn.BCEWithLogitsLoss(weight=class_weights)
+    #criterion = FocalLoss(alpha=0.75, gamma=0.5)
+    
+    criterion = nn.BCEWithLogitsLoss(weight=class_weights)
     
     optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.5)
