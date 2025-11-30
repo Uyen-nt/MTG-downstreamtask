56072ced68950b5e6ef2362e9c01dfec4cdc8d44
Uyen-nt <uyenntse170340@fpt.edu.vn>
2025-12-01 03:08:05 +0700

    Update model.py

--- behrt/model.py
+++ behrt/model.py
@@ -137,3 +137,22 @@ class BertForMultiLabelPrediction(Bert.modeling.BertPreTrainedModel):
             return loss, logits
         else:
             return logits
+
+
+class BertConfig(Bert.modeling.BertConfig):
+    def __init__(self, config):
+        super(BertConfig, self).__init__(
+            vocab_size_or_config_json_file=config.get('vocab_size'),
+            hidden_size=config['hidden_size'],
+            num_hidden_layers=config.get('num_hidden_layers'),
+            num_attention_heads=config.get('num_attention_heads'),
+            intermediate_size=config.get('intermediate_size'),
+            hidden_act=config.get('hidden_act'),
+            hidden_dropout_prob=config.get('hidden_dropout_prob'),
+            attention_probs_dropout_prob=config.get('attention_probs_dropout_prob'),
+            max_position_embeddings = config.get('max_position_embedding'),
+            initializer_range=config.get('initializer_range'),
+        )
+        self.seg_vocab_size = config.get('seg_vocab_size')
+        self.age_vocab_size = config.get('age_vocab_size')
+
